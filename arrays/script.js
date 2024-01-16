ee5f80f1ba3ceeb5727d59871103fdadba478bf3
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-01-16 15:36:07 +0200

    Array2,3

--- arrays/script.js
+++ arrays/script.js
@@ -3,3 +3,31 @@
 // function getUniqueArr(arr) {
 //   return Array.from(new Set(arr));
 // }
+
+// 2. Написать кастомный метод map -> customMap, первым параметром он принимает массив а вторым - функцию, как оригинальный метод map
+
+// function customMap(arr, func) {
+//   const customArr = [];
+//   for (const item of arr) {
+//     customArr.push(func(item));
+//   }
+
+//   for (let i = 0; i < arr.length; i++) {
+//     customArr.push(func(arr[i], i, arr));
+//   }
+
+//   return customArr;
+// }
+
+//  3. Написать кастомные метод reduce -> customReduce
+
+// function customReduce(arr, func, initial) {
+//   let accumulator = initial === undefined ? arr[0] : initial;
+
+//   const cycleCondition = initial === undefined ? 1 : 0;
+//   for (let i = cycleCondition; i < arr.length; i++) {
+//     accumulator = func(accumulator, arr[i], i, arr);
+//   }
+
+//   return accumulator;
+// }
